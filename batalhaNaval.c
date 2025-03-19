02b4cf2aad750efd1b6128fdec892dcf1ceb663d
okrhuan <doinrhuan@gmail.com>
2025-03-19 21:53:43 +0000

    Terminei o nivel aventireiro e coloquei os navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -16,6 +16,8 @@ int main() {
     // Coordenadas dos navios
     int navioh_linha = 2, navioh_coluna = 3; // Navio horizontal
     int naviov_linha = 5, naviov_coluna = 7; // Navio vertical
+    int naviod1_linha = 1, naviod1_coluna = 1;
+    int naviod2_linha = 1, naviod2_coluna =  8;
     int tamanho_navio = 3;
     
     // Colocando o horizontal
@@ -29,6 +31,20 @@ int main() {
     for (int i = 0; i < tamanho_navio; i++) {
         if (naviov_linha + i < 10) {
             tabuleiro[naviov_linha + i][naviov_coluna] = 3;
+        }
+    }
+
+    // Colocando o diagonal1
+    for (int k = 0; k < tamanho_navio ; k++){
+        if (naviod1_linha + k <10 && naviod1_coluna + k < 10){
+            tabuleiro[naviod1_linha + k][naviod1_coluna + k] = 3;
+        } 
+    }
+    
+    // Colocando o diagonal2
+    for (int l = 0; l < tamanho_navio ; l++){
+        if (naviod2_linha + l < 10 && naviod2_coluna - l >= 0){
+            tabuleiro[naviod2_linha + l][naviod2_coluna - l] = 3;
         }
     }
     
